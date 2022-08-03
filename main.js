5de8bb1c80e1bd21ed30ded457cfdfda2578b4f0
Raquel Costa <105187443+Raquelsc05@users.noreply.github.com>
2022-08-03 01:10:24 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-/*ScrollReveal: Mostrar elementos quando der scroll na página */
-
-
-
-
-ScrollReveal({  
-  origin: 'top',
-  distance: '30px',
-  duration: 700,
-  reset: true
-
-}).reveal(`
-#home, 
-#home .image,
-.text, 
-.profession, 
-#call button, #portifolio, #portifolio.img, #portifolio .desc, #skill, 
-#skill button, 
-.contact, 
-#contactme button, footer`);
-
-
 
 
 btn.addEventListener('click', function(){
